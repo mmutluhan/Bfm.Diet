15402b3b7b044852363105cb2df58b95c08bef49
Mehmet Ali Mutluhan <mmutluhan@gmail.com>
2021-01-24 01:39:32 +0300

    Commit

--- Bfm.Diet.Core/Extensions/StringExtension.cs
+++ Bfm.Diet.Core/Extensions/StringExtension.cs
@@ -266,26 +266,26 @@ namespace Bfm.Diet.Core.Extensions
             return '"' + GetEmptyStringIfNull(val).Replace("\"", "\"\"") + '"';
         }
 
-        public static string Encrypt(this string stringToEncrypt, string key)
-        {
-            var cspParameter = new CspParameters {KeyContainerName = key};
-            var rsaServiceProvider = new RSACryptoServiceProvider(cspParameter) {PersistKeyInCsp = true};
-            var bytes = rsaServiceProvider.Encrypt(Encoding.UTF8.GetBytes(stringToEncrypt), true);
-            return BitConverter.ToString(bytes);
-        }
+        //public static string Encrypt(this string stringToEncrypt, string key)
+        //{
+        //    var cspParameter = new CspParameters {KeyContainerName = key};
+        //    var rsaServiceProvider = new RSACryptoServiceProvider(cspParameter) {PersistKeyInCsp = true};
+        //    var bytes = rsaServiceProvider.Encrypt(Encoding.UTF8.GetBytes(stringToEncrypt), true);
+        //    return BitConverter.ToString(bytes);
+        //}
 
 
-        public static string Decrypt(this string stringToDecrypt, string key)
-        {
-            var cspParamters = new CspParameters {KeyContainerName = key};
-            var rsaServiceProvider = new RSACryptoServiceProvider(cspParamters) {PersistKeyInCsp = true};
-            var decryptArray = stringToDecrypt.Split(new[] {"-"}, StringSplitOptions.None);
-            var decryptByteArray = Array.ConvertAll(decryptArray,
-                s => Convert.ToByte(byte.Parse(s, NumberStyles.HexNumber)));
-            var bytes = rsaServiceProvider.Decrypt(decryptByteArray, true);
-            var result = Encoding.UTF8.GetString(bytes);
-            return result;
-        }
+        //public static string Decrypt(this string stringToDecrypt, string key)
+        //{
+        //    var cspParamters = new CspParameters {KeyContainerName = key};
+        //    var rsaServiceProvider = new RSACryptoServiceProvider(cspParamters) {PersistKeyInCsp = true};
+        //    var decryptArray = stringToDecrypt.Split(new[] {"-"}, StringSplitOptions.None);
+        //    var decryptByteArray = Array.ConvertAll(decryptArray,
+        //        s => Convert.ToByte(byte.Parse(s, NumberStyles.HexNumber)));
+        //    var bytes = rsaServiceProvider.Decrypt(decryptByteArray, true);
+        //    var result = Encoding.UTF8.GetString(bytes);
+        //    return result;
+        //}
 
         public static int CountOccurrences(this string val, string stringToMatch)
         {
